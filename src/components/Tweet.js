70478b422750e7fa0a2a671b1781022f6be2a32f
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-07 18:04:47 -0200

    Estilizando a listgem e usando o FlatList

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,3 +1,27 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export default class Tweet extends Component {
+  render() {
+
+    const { tweet } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.author}>{tweet.author}</Text>
+        <Text style={styles.content}>{tweet.content}</Text>
+
+        <TouchableOpacity onPress={() => {}} style={styles.like}>
+          <Icon name="ios-heart-empty" size={20} color="#999" />
+          <Text style={styles.likeText}>{tweet.likes}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
     padding: 20,
--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../services/api';
+
+import Tweet from '../components/Tweet';
 
 export default class Timeline extends Component {
 
   static navigationOptions = {
-    title: 'Timeline',
+    title: 'Tweets',
     headerRight: (
       <TouchableOpacity onPress={() => { }}>
         <Icon
@@ -31,13 +33,16 @@ export default class Timeline extends Component {
   render() {
     return (
       <View style={styles.container}>
-        {this.state.tweets.map(tweet => (
-          <Text>{tweet.author}</Text>
-        ))}
+        <FlatList
+          data={this.state.tweets}
+          keyExtractor={tweet => tweet._id}
+          renderItem={({ item }) => <Tweet tweet={item} />}
+        />
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
