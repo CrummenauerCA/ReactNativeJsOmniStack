aa841d68405672a9011e43a3e8f87be41e3f98b7
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-02-07 15:04:22 -0200

    Adicionando título e botão na barra de navegação

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
-
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Timeline extends Component {
+
+  static navigationOptions = {
+    title: 'Timeline',
+    headerRight: (
+      <TouchableOpacity onPress={() => { }}>
+        <Icon
+          style={{ marginRight: 20 }}
+          name="add-circle-outline"
+          size={24}
+          color="#4bb0ee"
+        />
+      </TouchableOpacity>
+    ),
+  }
+
   render() {
-    return(
+    return (
       <View style={styles.container}>
         <Text>Timeline</Text>
       </View>
